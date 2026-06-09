0d861445847ea37240cc01693efa96bdd8584590
Gabriel Carvalho <souza.gabrielscarvalho@gmail.com>
2026-06-09 11:30:49 -0300

    fix: alterar porta serial de COM7 para COM3

--- bh5100/bh5100.py
+++ bh5100/bh5100.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from shared.file_cleanup import FileCleanupConfig, start_cleanup_thread
 
 # ================= CONFIGURAÇÕES =================
-COM_PORT = 'COM7'
+COM_PORT = 'COM3'
 BAUD_RATE = 9600
 
 # ID da franquia configurado no banco de dados
